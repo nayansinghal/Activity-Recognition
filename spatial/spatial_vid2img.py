9335520229939ec8ac4e4b6e89bd59bbadd0a6a3
nayansinghal <singhalnayan91@gmail.com>
2017-04-11 22:38:30 -0500

    Creating images at 5frames per second

--- spatial/spatial_vid2img.py
+++ spatial/spatial_vid2img.py
@@ -11,7 +11,7 @@ from skvideo.io import vwrite
 
 def write_images():
 
-	root = 'sp_images_20/'
+	root = '../UCF-14/'
 
 	classes = open('../dataset/classInd_14.txt','r')
 	var1 = {}
@@ -23,7 +23,7 @@ def write_images():
 		for filename in files:
 			print filename
 			if ".DS_Store" not in filename:
-				folder = 'im_train' + '/' + filename.split('.')[0] + '/'   
+				folder = 'images' + '/' + filename.split('.')[0] + '/'
 				if not os.path.isdir(folder):
 					os.mkdir(folder)
 				else:
@@ -39,13 +39,13 @@ def write_images():
 						vid_name = filename.split('.')[0]
 						img_path = folder + vid_name + '_{}.jpg'.format(cnt + 1)
 						img_name = vid_name + '_{}'.format(cnt + 1)
-						print 'image name:', img_name
-						vwrite(img_path, frame)
-						cnt = cnt + 1
+						if fcnt % 5 == 0:
+							vwrite(img_path, frame)
+							cnt = cnt + 1
 						fcnt += 1
 
 					if cnt:
-						with open("count.txt", "a") as txt:
+						with open("count.txt", "w") as txt:
 							text = str(cnt) + " " + img_name.split('.')[0] + "\n"
 							txt.write(text)
 				except (RuntimeError, TypeError, NameError):
@@ -53,7 +53,7 @@ def write_images():
 
 def data_prep():
 
-	root = 'im_train/'
+	root = 'sp_images/test'
 	path = os.path.join(root, "")
 
 	classes = open('../dataset/classInd_14.txt','r')
@@ -62,21 +62,26 @@ def data_prep():
 		words = line.split(" ")
 		var1[words[1].split("\n")[0]] = words[0]
 
+	dic = {}
+	vidno = 0
+
 	for path, subdirs, files in os.walk(root):
 		for filename in files:
 			if ".DS_Store" not in filename:
 				frame_name = filename.split('.')[0]
 				idx = frame_name.rfind('_')
-				if (idx != -1) and (frame_name[:idx] in var1):
-					vidname = frame_name[:idx]
+				#print frame_name, idx, frame_name[:idx].split("_")[1]
+				if (idx != -1) and (frame_name[:idx].split("_")[1] in var1):
+					print frame_name
+					vidname = frame_name[:idx].split("_")[1]
 					dic[frame_name] = var1[vidname]
 					print vidno, 
 					vidno+=1
 
-	with open('./spatial_train_data_new.pickle', 'w') as f:
+	with open('./spatial_test_data_new.pickle', 'w') as f:
 		pickle.dump(dic, f)
 
 if __name__ == "__main__":
 	write_images()
 	gc.collect()
-	#data_prep()
+	data_prep()
